cd16e0468192c79240dfd3c127ac0b0e2256ece3
mayhmemo <mayhm.social@proton.me>
2025-12-06 23:32:21 -0300

    refactor: add useCallback to async storage hook functions

--- hooks/useAsyncStorage.ts
+++ hooks/useAsyncStorage.ts
@@ -1,22 +1,23 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useCallback } from "react";
 
 export function useAsyncStorage() {
-  const storeData = async (key: string, value: string) => {
+  const storeData = useCallback(async (key: string, value: string) => {
     try {
       await AsyncStorage.setItem(key, value);
     } catch (e) {
       console.log(e);
     }
-  };
+  }, []);
 
-  const getData = async (key: string) => {
+  const getData = useCallback(async (key: string) => {
     try {
       return await AsyncStorage.getItem(key);
     } catch (e) {
       console.log(e);
       return null;
     }
-  };
+  }, []);
 
   return {
     storeData,
